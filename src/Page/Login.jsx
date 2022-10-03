4fd4a0c7104482eec421950c8e23638a64440801
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-10-03 05:09:11 +0300

    create new pages
    create login and signup page :hand:

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,7 +1,35 @@
+import { FaGoogle } from 'react-icons/fa';
 export default function Login() {
-    return(
-        <div>
-            xd
+    return (
+        <div className="signup-home">
+            <div className="signup-content-cards">
+                <div className="signup-content-title">
+                    <span>
+                        Login
+                    </span>
+                </div>
+                <div className='inputs'>
+                    <div>
+                        <input placeholder="Username" type="text" />
+                    </div>
+                    <div>
+                        <input placeholder="Passworld" type="text" />
+                    </div>
+                </div>
+                <div className='buttons'>
+                    <button>Login</button>
+                </div>
+                <div className='passworld'>Forgot password?</div>
+                <div className='content-or'>
+                    <div style={{ borderTop: "solid white", width: "90px" }} ></div>
+                    <div style={{ marginTop: "-2rem" }} >Or</div>
+                    <div style={{ borderTop: "solid white", width: "90px" }} ></div>
+                </div>
+                <div className='google-login'>
+                    <div><FaGoogle /></div>
+                    <div>Continue with Google</div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Page/Signup.jsx
+++ src/Page/Signup.jsx
@@ -1,7 +1,38 @@
+import { FaGoogle } from 'react-icons/fa';
 export default function Signup() {
-    return(
-        <div>
-            xd
+    return (
+        <div className="signup-home">
+            <div className="signup-content-cards">
+                <div className="signup-content-title">
+                    <span>
+                        Login
+                    </span>
+                </div>
+                <div className='inputs'>
+                    <div>
+                        <input placeholder="Username" type="text" />
+                    </div>
+                    <div>
+                        <input placeholder="YourProfile.page/username" type="text" />
+                    </div>
+                    <div>
+                        <input placeholder="Passworld" type="password" />
+                    </div>
+                </div>
+                <div className='buttons'>
+                    <button>sign up with email</button>
+                </div>
+                <div className='passworld'>Forgot password?</div>
+                <div className='content-or'>
+                    <div style={{ borderTop: "solid white", width: "90px" }} ></div>
+                    <div style={{ marginTop: "-2rem" }} >Or</div>
+                    <div style={{ borderTop: "solid white", width: "90px" }} ></div>
+                </div>
+                <div className='google-login'>
+                    <div><FaGoogle  /></div>
+                    <div>Continue with Google</div>
+                </div>
+            </div>
         </div>
     )
 }
